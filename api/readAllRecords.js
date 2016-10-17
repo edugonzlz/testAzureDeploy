6cc5961e56d58cd8ab0cd112e45f3ca04e7a48c2
Eduardo González <fonologico@gmail.com>
2016-10-17 19:09:51 +0200

    commit con los cambios de Juanam

---
+++ api/readAllRecords.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Edu on 17/10/16.
+ */
+
+var api = {
+    get: function (req, res, next) {
+
+        // chequear parametros
+        if (typeof req.params.length < 0) {
+            return next();
+        }
+
+        var context = req.azureMobile;
+        var user = context.user.id;
+
+        var query = {
+            sql: "Select * FROM Autors"
+        };
+
+        req.azureMobile.data.execute(query)
+            .then(function (result) {
+                res.json(result);
+            });
+    }
+
+};
+
+api.get.access = 'authenticated';
+module.exports = api;
---
+++ tables/Autors.js
@@ -0,0 +1,46 @@
+/**
+ * Created by Edu on 17/10/16.
+ */
+
+var azureMobileApps = require('azure-mobile-apps');
+
+var table = azureMobileApps.table();
+
+table.columns = {
+    "name" : "string",
+    "secondName": "string"
+};
+
+
+table.dynamicSchema = false;
+
+/*
+ *   Trigger para insert
+ *
+ * */
+
+table.insert(function (context) {
+
+    context.item.usuario = context.user.id;
+    return context.execute();
+});
+
+
+table.read(function (context) {
+    context.query.where({usuario : context.user.id});
+    return context.execute();
+});
+
+/*
+ * Permisos de acceso a la tabla
+ *
+ * */
+
+
+table.read.access = 'anonymous';
+table.update.access = 'authenticated';
+table.delete.access = 'authenticated';
+table.insert.access = 'authenticated';
+
+
+module.exports = table;
