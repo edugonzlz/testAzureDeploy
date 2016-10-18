65a747766bf9815de36e780475b49733a922df94
Eduardo González <fonologico@gmail.com>
2016-10-18 11:48:53 +0200

    cambio menor

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var express = require("express"),
     azuremobileapps = require("azure-mobile-apps");
 
 var app = express(),
-    mobile = azuremobileapps( );
+    mobile = azuremobileapps();
 // { swagger: process.env.NODE_ENV !== 'production'}
 
 mobile.tables.import("./tables");
